a69fd7a342c49c78958252c5fbc2177f1f09b77c
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-08-27 18:12:57 +0200

    chore(bookmarks): prepare v0.2.2

--- packaging/python-bookmarks/src/shinyhub_bookmarks/__init__.py
+++ packaging/python-bookmarks/src/shinyhub_bookmarks/__init__.py
@@ -1,6 +1,6 @@
 """Selective URL bookmarking for Python Shiny apps hosted by ShinyHub."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from ._adapter import Field, Registration, register
 from ._dependency import bookmarking_dependency
